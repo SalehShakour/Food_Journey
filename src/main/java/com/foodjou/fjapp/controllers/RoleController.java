0e421eeee9ff922bdd8428acb54063d4ae56dfd9
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-22 13:23:30 +0330

    Created RoleController class and Added endpoints

--- src/main/java/com/foodjou/fjapp/controllers/RoleController.java
+++ src/main/java/com/foodjou/fjapp/controllers/RoleController.java
@@ -1,7 +1,58 @@
 package com.foodjou.fjapp.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.foodjou.fjapp.domain.Role;
+import com.foodjou.fjapp.services.RoleService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RoleController {
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostMapping("/api/roles")
+    public ResponseEntity<String> addRole(@RequestBody Role role) {
+        try {
+            roleService.addRole(role);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Role created successfully");
+        } catch (DataIntegrityViolationException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Role data is not valid");
+        }
+    }
+
+    @GetMapping("/api/roles/{id}")
+    public ResponseEntity<Role> getRoleById(@PathVariable String id) {
+        try {
+            Role role = roleService.getRoleById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(role);
+        } catch (DataIntegrityViolationException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/api/roles/{id}")
+    public ResponseEntity<String> deleteRoleById(@PathVariable String id) {
+        try {
+            roleService.deleteRoleById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Role deleted successfully");
+        } catch (EmptyResultDataAccessException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Role not found");
+        }
+    }
+
+    @PutMapping("/api/roles/{id}")
+    public ResponseEntity<String> updateRoleById(@PathVariable String id, @RequestBody Role updatedRole) {
+        try {
+            roleService.updateRoleById(id, updatedRole);
+            return ResponseEntity.status(HttpStatus.OK).body("Role updated successfully");
+        } catch (EmptyResultDataAccessException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
