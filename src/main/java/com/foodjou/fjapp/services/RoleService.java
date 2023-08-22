14db91c1a6b7254275234ef64124bb4b575fa2bb
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-22 13:22:15 +0330

    Created RoleService class and Added methods to retrieve roles by id

--- src/main/java/com/foodjou/fjapp/services/RoleService.java
+++ src/main/java/com/foodjou/fjapp/services/RoleService.java
@@ -1,6 +1,37 @@
 package com.foodjou.fjapp.services;
+import com.foodjou.fjapp.domain.Role;
+import com.foodjou.fjapp.repositories.RoleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public void addRole(Role role) {
+        roleRepository.save(role);
+    }
+
+    public Role getRoleById(String id) {
+        return roleRepository.findById(Long.valueOf(id)).orElse(null);
+    }
+
+    public void deleteRoleById(String id) {
+        roleRepository.deleteById(Long.valueOf(id));
+    }
+
+    public void updateRoleById(String id, Role updatedRole) {
+        Role existingRole = roleRepository.findById(Long.valueOf(id)).orElse(null);
+        if (existingRole != null) {
+            // Update the fields of existingRole using updatedRole
+            existingRole.setRoleName(updatedRole.getRoleName());
+            existingRole.setDescription(updatedRole.getDescription());
+            // Save the updated role
+            roleRepository.save(existingRole);
+        }
+    }
 }
+
