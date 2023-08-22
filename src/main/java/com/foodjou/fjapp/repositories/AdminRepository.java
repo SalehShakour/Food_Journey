9efa965087bfac1f6006be6d5b281f030256a4ce
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-22 13:09:13 +0330

    Handle Admin Deletion with Role-Based Access Control

--- src/main/java/com/foodjou/fjapp/repositories/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodjou.fjapp.repositories;
-
-import com.foodjou.fjapp.domain.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin,String> {
-}
--- src/main/java/com/foodjou/fjapp/services/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.foodjou.fjapp.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminService {
-}
