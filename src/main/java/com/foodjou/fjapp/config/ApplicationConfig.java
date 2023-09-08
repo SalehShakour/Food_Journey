1b3b4333d4d838a7f6cd7367502e545497542cdf
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-09-08 19:11:31 +0330

    save uncompleted changes

--- src/main/java/com/foodjou/fjapp/config/ApplicationConfig.java
+++ src/main/java/com/foodjou/fjapp/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package com.foodjou.fjapp.config;
 
 import com.foodjou.fjapp.repositories.UserRepository;
-import com.foodjou.fjapp.services.FoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
