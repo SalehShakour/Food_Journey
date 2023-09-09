5e559c4fc848f81b991c930a889a0142eb3eb24e
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-09-09 13:50:14 +0330

    set DeliveryMode to Persistent for rabbitMQ Message

--- src/main/java/com/foodjou/fjapp/rabbitmq/RabbitMQProducer.java
+++ src/main/java/com/foodjou/fjapp/rabbitmq/RabbitMQProducer.java
@@ -4,6 +4,9 @@ import com.foodjou.fjapp.controllers.FoodController;
 import com.foodjou.fjapp.domain.Food;
 import com.foodjou.fjapp.domain.User;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,11 +26,14 @@ public class RabbitMQProducer {
 
     private final RabbitTemplate rabbitTemplate;
     public void sendFoodLog(Food food, User currentUser){
-        String message = String.format("Received a request to get food with ID: %d. Requester: %s. Timestamp: %s",
+        String textMessage = String.format("Received a request to get food with ID: %d. Requester: %s. Timestamp: %s",
                 food.getId(), currentUser.getUsername(), LocalDateTime.now());
-        LOGGER.info(message);
+        LOGGER.info(textMessage);
         String exchange = "exchange_demo";
         String routingKey = "routing_key_demo";
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);
+        Message message = new Message(textMessage.getBytes(), messageProperties);
         rabbitTemplate.convertAndSend(exchange, routingKey, message);
     }
 }
