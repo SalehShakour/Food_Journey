b4ae9863983e5666435868c67c3d7a7594878868
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-09-10 12:53:48 +0330

    send Log as DTO

--- src/main/java/com/foodjou/fjapp/rabbitmq/RabbitMQProducer.java
+++ src/main/java/com/foodjou/fjapp/rabbitmq/RabbitMQProducer.java
@@ -1,9 +1,14 @@
 package com.foodjou.fjapp.rabbitmq;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.foodjou.fjapp.domain.Food;
 import com.foodjou.fjapp.domain.Order;
 import com.foodjou.fjapp.domain.User;
+import com.foodjou.fjapp.rabbitmq.CommonDTOs.*;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageDeliveryMode;
@@ -17,48 +22,42 @@ import java.time.LocalDateTime;
 @Service
 @AllArgsConstructor
 public class RabbitMQProducer {
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
 
     private final RabbitTemplate rabbitTemplate;
 
-
-    public void sendFoodLog(Food food, User currentUser){
-        String textMessage = String.format("Received a request to get food with ID: %d. Requester: %s. Timestamp: %s",
-                food.getId(), currentUser.getUsername(), LocalDateTime.now());
-        LOGGER.info(textMessage);
+    public void sendFoodLog(Food food, User currentUser) {
         String routingKey = "routing_key_food";
-        sendLog(routingKey, textMessage);
+        sendLog(routingKey, new ViewedEntityLogDTO(food.getId()
+                ,currentUser.getUsername(),
+                LocalDateTime.now()));
+
     }
 
-    public void sendOrderLog(Order order, User currentUser){
-        String textMessage = String.format("Received a request to get order with ID: %d. Requester: %s. Timestamp: %s",
-                order.getId(), currentUser.getUsername(), LocalDateTime.now());
-        LOGGER.info(textMessage);
-        String routingKey = "routing_key_order";
-        sendLog(routingKey, textMessage);
-    }
 
-    public void sendUserLog(User user){
-        String textMessage = String.format("Received a request to get user with ID: %d. Timestamp: %s",
-                user.getId(), LocalDateTime.now());
-        LOGGER.info(textMessage);
-        String routingKey = "routing_key_user";
-        sendLog(routingKey, textMessage);
-    }
-
-    public void sendRestaurantLog(Long restaurantId, User currentUser){
-        String textMessage = String.format("Received a request to get restaurant with ID: %d. Requester: %s. Timestamp: %s",
-                restaurantId, currentUser.getUsername(), LocalDateTime.now());
-        LOGGER.info(textMessage);
+    public void sendRestaurantLog(Long restaurantId, User currentUser) {
         String routingKey = "routing_key_restaurant";
-        sendLog(routingKey, textMessage);
+        sendLog(routingKey, new ViewedEntityLogDTO(restaurantId
+                ,currentUser.getUsername(),
+                LocalDateTime.now()));
+
     }
 
-    private void sendLog(String routingKey, String textMessage){
+
+    private void sendLog(String routingKey, Object payload) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        byte[] payloadBytes;
+        try {
+            payloadBytes = mapper.writeValueAsBytes(payload);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         MessageProperties messageProperties = new MessageProperties();
         messageProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);
-        Message message = new Message(textMessage.getBytes(), messageProperties);
+        Message message = new Message(payloadBytes, messageProperties);
         rabbitTemplate.convertAndSend("myTopicExchange", routingKey, message);
     }
+
 }
 
